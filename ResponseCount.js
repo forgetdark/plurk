737008871fba5882e27c53a1d4099af4e62a8846
forgetdark <forgetdark@gmail.com>
2022-02-15 19:40:12 +0800

    Update ResponseCount.js

--- ResponseCount.js
+++ ResponseCount.js
@@ -1,4 +1,124 @@
 javascript:(function(){
+  var $loader = (function () {
+    var loaderStyle = document.createElement('style');
+    loaderStyle.id = 'loader-style';
+    loaderStyle.innerHTML = `.loader-overlay {
+      visibility: hidden;
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 999999;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.6);
+    }
+    .loader-model {
+      visibility: hidden;
+      position: relative;
+      width: 2.5em;
+      height: 2.5em;
+      transform: rotate(165deg);
+      z-index: 99999999;
+    }
+    .loader-model:before,
+    .loader-model:after {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      display: block;
+      width: 0.5em;
+      height: 0.5em;
+      border-radius: 50px;
+      transform: translate(-50%, -50%);
+    }
+    .loader-model:before {
+      animation: before 2s infinite;
+    }
+    .loader-model:after {
+      animation: after 2s infinite;
+    }
+    @keyframes before {
+      0% {
+        width: 0.5em;
+        box-shadow:
+          1em -0.5em rgb(255, 238, 170),
+          -1em 0.5em rgb(204, 170, 136);
+      }
+      35% {
+        width: 2.5em;
+        box-shadow:
+          0 -0.5em rgb(255, 238, 170),
+          0 0.5em rgb(204, 170, 136);
+      }
+      70% {
+        width: 0.5em;
+        box-shadow:
+          -1em -0.5em rgb(255, 238, 170),
+          1em 0.5em rgb(204, 170, 136);
+      }
+      100% {
+        box-shadow:
+          1em -0.5em rgb(255, 238, 170),
+          -1em 0.5em rgb(204, 170, 136);
+      }
+    }
+    @keyframes after {
+      0% {
+        height: 0.5em;
+        box-shadow:
+          0.5em 1em rgb(136, 136, 204),
+          -0.5em -1em rgb(255, 136, 187);
+      }
+      35% {
+        height: 2.5em;
+        box-shadow:
+          0.5em 0 rgb(136, 136, 204),
+          -0.5em 0 rgb(255, 136, 187);
+      }
+      70% {
+        height: 0.5em;
+        box-shadow:
+          0.5em -1em rgb(136, 136, 204),
+          -0.5em 1em rgb(255, 136, 187);
+      }
+      100% {
+        box-shadow:
+          0.5em 1em rgb(136, 136, 204),
+          -0.5em -1em rgb(255, 136, 187);
+      }
+    }
+    .loader-model {
+      position: fixed;
+      top: calc(50% - 2.5em / 2);
+      left: calc(50% - 2.5em / 2);
+    }`;
+    var loaderOverlay = document.createElement('div');
+    loaderOverlay.classList.add('loader-overlay');
+    var loaderModel = document.createElement('div');
+    loaderModel.classList.add('loader-model');
+    return {
+      show: function () {
+        document.body.appendChild(loaderStyle);
+        document.body.appendChild(loaderOverlay);
+        document.body.appendChild(loaderModel);
+        document.querySelector('.loader-overlay').style.visibility = 'visible';
+        document.querySelector('.loader-model').style.visibility = 'visible';
+      },
+      hide: function () {
+        document.getElementById('loader-style').remove();
+        document.querySelector('.loader-overlay').remove();
+        document.querySelector('.loader-model').remove();
+      }
+    };
+  })();
+
+  var $toTop = function (topEl) {
+    window.document.documentElement.scrollTop = topEl.offsetTop;
+    window.pageYOffset = topEl.offsetTop;
+    document.body.scrollTop = topEl.offsetTop;
+  };
+
   var $createResult = function (totalCount, allResult) {
     var style = document.createElement('style');
     style.innerHTML = `
@@ -35,6 +155,7 @@ javascript:(function(){
   };
 
   var $getResponse = function () {
+    $loader.show();
     document.querySelector('.load-all-older').click();
     return new Promise(function (resolve, reject) {
       var startInterval = setInterval(function(){
@@ -111,7 +232,8 @@ javascript:(function(){
         document.querySelector('.search-sel').value = '';
       });
 
-      window.document.documentElement.scrollTop = 0;
+      $toTop(document.getElementById('result-info'));
+      $loader.hide();
     });
   }
 })();
